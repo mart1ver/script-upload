44390f98b0312ce3c7d251ae379243f5d31c66f8
Vert martin <mart1ver@gmail.com>
2017-03-03 23:31:36 +0100

    config

--- ifaces/index.php
+++ ifaces/index.php
@@ -78,9 +78,9 @@
  
 // Constantes
 define('TARGET', '../files/');    // Repertoire cible
-define('MAX_SIZE', 100000);    // Taille max en octets du fichier
-define('WIDTH_MAX', 800);    // Largeur max de l'image en pixels
-define('HEIGHT_MAX', 800);    // Hauteur max de l'image en pixels
+define('MAX_SIZE', 1000000);    // Taille max en octets du fichier
+define('WIDTH_MAX', 2000);    // Largeur max de l'image en pixels
+define('HEIGHT_MAX', 2000);    // Hauteur max de l'image en pixels
  
 // Tableaux de donnees
 $tabExt = array('jpg','gif','png','jpeg');    // Extensions autorisees
