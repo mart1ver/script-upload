84585394e6a71248ab781109b82cd5cdff515a00
Vert martin <mart1ver@gmail.com>
2017-03-04 17:59:44 +0100

    utf8 merci

--- ifaces/index.php
+++ ifaces/index.php
@@ -60,6 +60,7 @@ require_once('../moteur/uploadConfig.php');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
   <head>
+  <meta charset="UTF-8">
     <title>Upload d une image sur le serveur </title>
   </head>
   <body>
